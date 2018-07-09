09aae071c1ca669f3354a68de1ad8f6447e0c6a0
Mantissa-23 <dylanculfogienis@gmail.com>
2018-07-09 09:36:53 -0400

    Fixed issue with gulp-hub not correctly importing gulpfiles

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,26 @@ global.wikibrick = require('./config')(path.resolve(__dirname)); // Pass in loca
 var config = global.wikibrick;
 
 // Require default igem-wikibrick tasks
+//hub([path.resolve('./node_modules/igem-wikibrick/gulp/tasks/unit/*.js')])
+//hub([path.resolve('./node_modules/igem-wikibrick/gulp/tasks/compound/*.js')])
+
+// Require unit tasks
+//hub([config.gulp.unit]);
+
+// Require compound tasks once unit tasks are defined
+//hub([config.gulp.compound]);
+/*
+var HubRegistry = require('gulp-hub');
+var hub = new HubRegistry([
+  './node_modules/igem-wikibrick/gulp/tasks/unit/*.js',
+  './node_modules/igem-wikibrick/gulp/tasks/compound/*.js',
+  config.gulp.unit,
+  config.gulp.compound
+]);
+
+gulp.registry(hub);
+*/
+// Require default igem-wikibrick tasks
 hub([path.resolve('./node_modules/igem-wikibrick/gulp/tasks/unit/*.js')])
 hub([path.resolve('./node_modules/igem-wikibrick/gulp/tasks/compound/*.js')])
 
@@ -15,8 +35,3 @@ hub([config.gulp.unit]);
 
 // Require compound tasks once unit tasks are defined
 hub([config.gulp.compound]);
-
-gulp.task('die', function(done) {
-  console.log('die');
-  done();
-})
