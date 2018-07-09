81c3b7536b251ed2adaa4edda99f115d0e86dcd5
Mantissa-23 <dylanculfogienis@gmail.com>
2018-07-09 13:25:19 -0400

    Gulp serve now works!

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp');
+var gulp = require('igem-wikibrick/gulpfile');
 var hub = require('gulp-hub');
 var path = require('path');
 
@@ -6,32 +6,4 @@ global.wikibrick = require('./config')(path.resolve(__dirname)); // Pass in loca
 
 var config = global.wikibrick;
 
-// Require default igem-wikibrick tasks
-//hub([path.resolve('./node_modules/igem-wikibrick/gulp/tasks/unit/*.js')])
-//hub([path.resolve('./node_modules/igem-wikibrick/gulp/tasks/compound/*.js')])
-
-// Require unit tasks
-//hub([config.gulp.unit]);
-
-// Require compound tasks once unit tasks are defined
-//hub([config.gulp.compound]);
-/*
-var HubRegistry = require('gulp-hub');
-var hub = new HubRegistry([
-  './node_modules/igem-wikibrick/gulp/tasks/unit/*.js',
-  './node_modules/igem-wikibrick/gulp/tasks/compound/*.js',
-  config.gulp.unit,
-  config.gulp.compound
-]);
-
-gulp.registry(hub);
-*/
-// Require default igem-wikibrick tasks
-hub([path.resolve('./node_modules/igem-wikibrick/gulp/tasks/unit/*.js')])
-hub([path.resolve('./node_modules/igem-wikibrick/gulp/tasks/compound/*.js')])
-
-// Require unit tasks
-hub([config.gulp.unit]);
-
-// Require compound tasks once unit tasks are defined
-hub([config.gulp.compound]);
+//console.log(config.browsersync.development.server);
