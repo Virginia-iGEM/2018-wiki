c204634da4f47ac203ccf3ee9eb7d72989070a03
Mantissa-23 <dylanculfogienis@gmail.com>
2018-07-11 13:19:28 -0400

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-var gulp = require('igem-wikibrick/gulpfile'); // Require gulp from the igem-wikibrick module
 var path = require('path'); // Need path to to absolutify dirname
 
 // Pass in local directory name, needed for configuration generation
 // for igem-wikibrick. This same configuration file is made a global
 // variable, so you can use it in your own gulp tasks.
 global.wikibrick = require('./config')(path.resolve(__dirname)); 
+// Require gulp from the igem-wikibrick module
+// Pass in configuration
+var gulp = require('igem-wikibrick/gulp/gulpfile')(global.wikibrick); 
