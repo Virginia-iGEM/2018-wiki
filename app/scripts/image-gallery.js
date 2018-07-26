76bb85e3836e2ac42878ff7af498ef831f398341
Grace Wu <gw5cc@virginia.edu>
2018-07-26 12:26:30 -0400

    delete your favorite team members

--- app/scripts/image-gallery.js
+++ app/scripts/image-gallery.js
@@ -1,3 +1,15 @@
-$('#image_gallery img').click(function(){
 
+$(document).ready(function() {
+    $("img").click(function(event) {
+        var id = event.target.id;
+        //alert(id);
+        var text = document.getElementById(id);
+        if(text.style.display == "none"){
+        	text.style.display = "block";
+        }
+        else{
+        	text.style.display = "none";
+        }
+        }
+    );
 });
