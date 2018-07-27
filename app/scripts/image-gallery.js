e8f6d136611e2bcbd1e40e05086c19bd3c8d2e4e
Grace Wu <gw5cc@virginia.edu>
2018-07-27 15:08:28 -0400

    added temp bios to team page

--- app/scripts/image-gallery.js
+++ app/scripts/image-gallery.js
@@ -1,21 +1,27 @@
 
 $(document).ready(function() {
+	var previous = " ";
     $("img").click(function(event) {
         var id_name = event.target.id;
-        id_name = id_name.replace(/[0-9]/g, '');
-        //alert(id_name);
-        var target_name = $(`.${id_name}`).id;
-        unhide(id_name);
+        id_name = id_name.replace(/[0-9]/g, ''); //regex for alphabets, no numbers
+        var id_toggle = document.getElementById(id_name); //get ID of names under class "team-bio"
+        if(document.getElementById(id_name)){ //if ID exists
+			if((previous != id_name) && (previous != " ")){ //if you click a diff image
+				console.log(previous);
+				var id_previous = document.getElementById(previous);
+				$(id_previous).toggle("hidden");
+				console.log(id_previous);
+
+			}
+			else if(previous == id_name){ //clicking on the same image
+				$(id_toggle).toggle("unhidden");
+			}
+			$(id_toggle).toggle("unhidden"); //toggles txtbox to appear
+        	 previous = id_name;//checks to see if you clicked different images
+        }
+        else{
+        	alert("This person does not exist. Try again.");
+        }
         }
     );
 });
-
-var previous = "";
-
-function unhide(divID) {
-	if(divID) {
-		divID.className=(divID.className=='hidden')?'unhidden':'hidden';
-		previous.className=(previous.className=='hidden')?'unhidden':'hidden';
-		previous=divID;
-	}
-}
