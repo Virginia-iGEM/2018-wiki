830cf7c694a76eb7f1774801deb94530b4d88424
Mantissa-23 <dylanculfogienis@gmail.com>
2018-08-14 12:31:23 -0400

    Table of contents functions but is not sticky

--- app/scripts/navline.js
+++ app/scripts/navline.js
@@ -2,7 +2,6 @@
 // https://stackoverflow.com/a/187946
 
 window.onload = function() {
-    console.log("bliegh");
     var toc = "";
     var level = 0;
 
@@ -33,9 +32,37 @@ window.onload = function() {
         toc += (new Array(level +1)).join("</ul>");
     }
 
-    console.log(toc);
-
     document.getElementById("toc").innerHTML += toc;
+};
 
-    console.log("done!");
+window.onscroll = function() {
+    var smallestoffset = -$(document).height();
+    var smallestheader;
+
+    $('article h1').each(function(i) { 
+        var offset = $(this).position().top - $(document).scrollTop() - Math.max(document.documentElement.clientHeight, window.innerHeight || 0)/2; // Negative offset values indicate the header's top has passed the midpoint of the user's view
+        //console.log($(this).text() + ' ' + offset);
+        if (offset > smallestoffset && offset <= 0) {
+            smallestoffset = offset;
+            smallestheader = $(this);
+        }
+    });
+
+    if (smallestheader === undefined) {
+        smallestheader = $('article h1:first');
+    }
+
+    //console.log(smallestheader.text());
+
+    $('#toc>ul>li').each(function(i) {
+        //console.log($(this).children('a').attr('href'));
+        //console.log('#' + smallestheader.attr('id'));
+        if ($(this).children('a').attr('href') === '#' + smallestheader.attr('id')) {
+            $(this).children('ul').addClass('current');
+        }
+        else {
+            $(this).children('ul').removeClass('current');
+        }
+    });
+    //console.log($(document).scrollTop());
 };
