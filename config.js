cc500af5340355283996482028c1df13a9f08e74
Mantissa-23 <dylanculfogienis@gmail.com>
2018-08-15 10:50:39 -0400

    Temporarily disabled minification due to gulp-csso error

--- config.js
+++ config.js
@@ -89,7 +89,7 @@ module.exports = function(root) {
         },
         live: {
             banner: true,
-            minify: true,
+            minify: false,
             relative2absolute: true,
             serve: false
         }
