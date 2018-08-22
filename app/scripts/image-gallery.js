7e4ab1745b9ade4e5d6c93fe15b829a64dc000a9
Grace Wu <gw5cc@virginia.edu>
2018-08-22 15:45:13 -0400

    Fixed team page WOHOOOOOO

--- app/scripts/image-gallery.js
+++ app/scripts/image-gallery.js
@@ -4,7 +4,6 @@ jq(document).ajaxStop(function () {
     'use strict';
     
     var previous = ' ';
-    console.log('asdf');
 
     jq(document).on('click', '#image_gallery', function(event) {
         var id_name = event.target.id;
@@ -12,19 +11,20 @@ jq(document).ajaxStop(function () {
         id_name = id_name.replace(/[0-9]/g, ''); //regex for alphabets, no numbers
         var id_toggle = document.getElementById(id_name); //get ID of names under class 'team-bio'
 
-        if (document.getElementById(id_name)) { //if ID exists
-            if ((previous != id_name) && (previous != ' ')) { //if you click a diff image
+        if (document.getElementById(id_name) && id_name != "image_gallery") { //if ID exists and accounting for white space
+            if ((previous != id_name)) { //if you click a diff image
                 var id_previous = document.getElementById(previous);
-                jq(id_previous).toggle('hidden');
-                console.log(id_previous);
-
-            } else if (previous == id_name) { //clicking on the same image
-                jq(id_toggle).toggle('unhidden');
+                jq(id_previous).hide();
+                //console.log(id_previous);
+            } 
+            else if (previous == id_name) { //clicking on the same image
+                jq(id_toggle).show();
             }
-            jq(id_toggle).toggle('unhidden'); //toggles txtbox to appear
+            jq(id_toggle).show(); //show bio
             previous = id_name; //checks to see if you clicked different images
-        } else {
-            alert('This person does not exist. Try again.');
+        } 
+        else {
+            jq(previous).show();
         }
     });
 });
