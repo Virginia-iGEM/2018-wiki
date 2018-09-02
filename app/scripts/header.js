804736b0fb45bc860a136c7ea233b3effb731e69
Mantissa-23 <dylanculfogienis@gmail.com>
2018-09-01 23:52:56 -0400

    Basic hamburger menu implemented

--- app/scripts/header.js
+++ app/scripts/header.js
@@ -2,33 +2,38 @@
 // https://codepen.io/Mhmdhasan/pen/mAdaQE
 
 $(document).ready(function () {
-  
-  'use strict';
-  
-   var c, currentScrollTop = 0,
-       navbar = $('header');
-
+    
+    'use strict';
+    
+    var c, currentScrollTop = 0,
+        navbar = $('header');
     var hovered = false;
 
     navbar.hover(function() {
         hovered = true;
     },
-    function() {
-        hovered = false;
-    });
+                 function() {
+                     hovered = false;
+                 });
 
-   $(window).scroll(function () {
-      var a = $(window).scrollTop();
-      var b = navbar.height();
-     
-      currentScrollTop = a;
-     
-      if (c < currentScrollTop && a > b + b && navbar && !hovered) {
-        navbar.addClass("scrollUp");
-      } else if (c > currentScrollTop && !(a <= b)) {
-        navbar.removeClass("scrollUp");
-      }
-      c = currentScrollTop;
-  });
-  
+    // Handle hiding the navbar when scrolling
+    $(window).scroll(function () {
+        var a = $(window).scrollTop();
+        var b = navbar.height();
+        
+        currentScrollTop = a;
+        
+        if (c < currentScrollTop && a > b + b && navbar && !hovered) {
+            navbar.addClass("scrollUp");
+        } else if (c > currentScrollTop && !(a <= b)) {
+            navbar.removeClass("scrollUp");
+        }
+        c = currentScrollTop;
+    });
 });
+
+// Hamburger menu button for mobileview
+function openMenu() {
+    navbar = $('#navbar');
+    navbar.toggleClass('expanded');
+}
