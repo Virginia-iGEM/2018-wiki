fa0bed39c215ca6315baed34e41918ed4fb432fb
Mantissa-23 <dylanculfogienis@gmail.com>
2018-09-17 16:03:08 -0400

    Continued work on landing page

---
+++ app/scripts/interactive.js
@@ -0,0 +1,17 @@
+d3 = require('d3');
+
+$(document).ajaxStop(function() {
+    var sampleSVG = d3.select(".petri")
+        .append("svg")
+        .attr("width", 100)
+        .attr("height", 100);    
+
+    sampleSVG.append("circle")
+        .style("stroke", "gray")
+        .style("fill", "white")
+        .attr("r", 40)
+        .attr("cx", 50)
+        .attr("cy", 50)
+        .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+        .on("mouseout", function(){d3.select(this).style("fill", "white");});
+});
