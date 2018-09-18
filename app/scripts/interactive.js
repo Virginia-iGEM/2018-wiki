c5b158e758652257c0311b53f5c7509219164ad5
Mantissa-23 <dylanculfogienis@gmail.com>
2018-09-18 13:14:28 -0400

    Continued work on mini sim

--- app/scripts/interactive.js
+++ app/scripts/interactive.js
@@ -36,35 +36,78 @@ $(document).ajaxStop(function() {
         tick = 5,
         Ny = Math.floor(dim / (gridSpacing * Math.sin(Math.PI / 3))),
         Nx = Math.floor(dim / gridSpacing),
-        sim = cellSim(Nx, Ny),
+        divprob = 0.1,
+        sim = cellSim(Nx, Ny, divprob),
         display = cellDisplay(),
         radius = delta/2 - 2,
         grid = d3.map();
     
-    draw();
-    load();
-    d3.timeout(redraw);
+    display.draw();
+    sim.init();
+    sim.loadRandom();
+    d3.timeout(display.draw);
 
-    function cellSim() {
+
+    function cellSim(N, M, dp) {
         var cells = d3.map();
         var inactive = d3.map();
         var active = d3.map();
             
         return {
-            init: init,
-            step: step,
-            add: add
+            loadRandom: loadRandom,
+            reset: reset, // Initialize the simulation; resets
+            step: step, // One step of simulation
+            add: add // Add cells to the simulation
         };
 
-        function init(data) {
+        function loadRandom(p) {
+            var n = Math.floor((p || 0.05)*N*M), // How many cells are we loading in
+                i = d3.randomUniform(0,  N-1), // Randomly generate positions on the X axis
+                j = d3.randomUniform(1, M), // Randomly generate positions on the Y axis
+                d;
 
+            for (var k=0; k < n; k++) {
+                d = [Math.floor(i()) + 1, Math.floor(j()) + 1];
+                cells.set(d, d);
+                inactive.set(d, d);
+            }
+        }
+
+        function reset(data) {
+            cells.clear();
         }
 
         function step() {
+            if (cells.size() > 0) {
+                cells.each(function (cell, key) {
+                    
+                });
+            }
+        }
+
+        function add() { // TBI: Add a single cell
 
         }
 
-        function add() {
+        function neighbors(c) {
+            // Key:
+            // 0: [n - 1, m + 1]
+            // 1: [n, m + 1]
+            // 2: [n + 1, m]
+            // 3: [n, m - 1]
+            // 4: [n - 1, m - 1]
+            // 5: [n - 1, m]
+
+            var n = c[0],
+                m = c[1];
+            return [
+                cells[n - 1, m + 1],
+                cells[n, m + 1],
+                cells[n + 1, m],
+                cells[n, m - 1],
+                cells[n - 1, m - 1],
+                cells[n - 1, m]
+            ];
         }
     }
     
@@ -77,6 +120,7 @@ $(document).ajaxStop(function() {
             d_cells: d_cells,
             d_active: d_active
         };
+
 
         function init() {
             d_inactive = petri.append('g').classed('cell', true);
@@ -113,7 +157,12 @@ $(document).ajaxStop(function() {
                 .attr('cy', function (d) { // Set y coordinate
                 });
 
-            d3.timeout(draw, tick);
+        }
+
+        function redraw() {
+            sim.step();
+            draw();
+            d3.timeout(redraw, tick);
         }
     }
 });
