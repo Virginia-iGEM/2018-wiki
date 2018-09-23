a7c26a317a0a1322e88030fd741ee2a3599b4280
Mantissa-23 <dylanculfogienis@gmail.com>
2018-09-23 18:51:41 -0400

    CHECK OUT MY CELLS BITCHES

--- app/scripts/interactive.js
+++ app/scripts/interactive.js
@@ -15,7 +15,7 @@ $(document).ajaxStop(function() {
       .append("svg")
       .attr("width", 100)
       .attr("height", 100);    
-      */
+    */
 
     var interactive = d3.select(".petri");
 
@@ -25,6 +25,108 @@ $(document).ajaxStop(function() {
         .attr("width", dim)
         .attr("height", dim);
 
+    var nodes_data = d3.range(500).map(function(i) {
+        return {index: i, radius: (i%3)*5};
+    });
+    //set up the simulation 
+    //nodes only for now 
+    var simulation = d3.forceSimulation()
+        .nodes(nodes_data);
+
+    //add forces
+    //we're going to add a charge to each node 
+    //also going to add a centering force
+    simulation
+        //.gravity(0.05)
+        .force("charge_force", d3.forceManyBody().strength(5))
+        .force("collision", d3.forceCollide(function(d) {return d.radius + 4;}))
+        .force("center_force", d3.forceCenter(dim / 2, dim / 2));
+
+
+    var drag = simulation => {
+        
+        function dragstarted(d) {
+            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+        
+        function dragged(d) {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        }
+        
+        function dragended(d) {
+            if (!d3.event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
+        
+        return d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+    };
+    //draw circles for the nodes 
+    var node = petri.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(nodes_data)
+        .enter()
+        .append("circle")
+        .attr("r", function(d) {return d.radius;})
+        .attr("fill", "red")
+        .call(drag(simulation));
+
+    simulation.on("tick", tickActions );
+
+    /*
+    //Create links data 
+    var links_data = [
+        {"source": "Travis", "target": "Rake"},
+        {"source": "Diana", "target": "Rake"},
+        {"source": "Diana", "target": "Rachel"},
+        {"source": "Rachel", "target": "Rake"},
+        {"source": "Rachel", "target": "Shawn"},
+        {"source": "Emerald", "target": "Rachel"}
+    ];
+
+    //Create the link force 
+    //We need the id accessor to use named sources and targets 
+    var link_force =  d3.forceLink(links_data)
+        .id(function(d) { return d.name; });
+
+
+    simulation.force("links",link_force);
+
+    //draw lines for the links 
+    var link = petri.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(links_data)
+        .enter().append("line")
+        .attr("stroke-width", 2);
+
+        */
+
+    function tickActions() {
+        //update circle positions each tick of the simulation 
+        node
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+        
+        /*
+        //update link positions 
+        //simply tells one end of the line to follow one node around
+        //and the other end of the line to follow the other node around
+        link
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+        */
+    }
+    /*
     var gridSpacing = 10, // Repeat distance in pixels between cells
         tick = 500,
         Ny = Math.floor(dim / (gridSpacing * Math.sin(Math.PI / 3))),
@@ -109,4 +211,5 @@ $(document).ajaxStop(function() {
         draw();
         d3.timeout(redraw, tick);
     }
+    */
 });
