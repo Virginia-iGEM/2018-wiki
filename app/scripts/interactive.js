24b165ef354a3026d7ac72b962f5b37379443ef3
Mantissa-23 <dylanculfogienis@gmail.com>
2018-09-24 22:13:37 -0400

    THEY'RE SO PRETTY

--- app/scripts/interactive.js
+++ app/scripts/interactive.js
@@ -7,15 +7,168 @@
  * Luca Spardella's D3 Game of Life implementation.
  */
 
-d3 = require('d3');
+var d3 = require('d3');
+//var jz = require('jeezy');
 
 $(document).ajaxStop(function() {
-    /*
-      var sampleSVG = d3.select(".petri")
-      .append("svg")
-      .attr("width", 100)
-      .attr("height", 100);    
+    // Set interactive height to be equal to the width
+    $('.petri').css('height', $('.petri').css('width'));
+
+    // Grab our <div> container
+    var container = d3.select('.petri');
+    var dim = parseInt(container.style('width'));
+
+    var cellRadius = 10; // Global of cells in pixels
+
+    var nodes = [{radius: cellRadius}]; // Start with just one cell
+    
+    // Establish data - this is what our simulation and display draws from
+    // Establish simulation - this handles the physics portion of things
+    var simulation = d3.forceSimulation(nodes)
+        .force('charge', d3.forceManyBody().strength(-30)) // Repulse cells 
+        .force('center', d3.forceCenter()) // Push cells towards center
+        .force('collision', d3.forceCollide(function(d) {return d.radius + 2;})) // Make cells collide with each other
+        .alphaTarget(1) // Make solver settle more quickly
+        .on('tick', ticked); // Call any functions that need to run every time the simulation ticks
+
+    // Establish SVG drawing
+    var svg = container.append('svg').attr('width', dim).attr('height', dim);
+    // Add styling to center container
+    var g = svg.append('g').attr('transform', 'translate(' + dim / 2 + ',' + dim / 2 + ')');
+    // This is the group we'll actually draw cells to
+    var node = g.append('g').selectAll('.node').attr('class', 'nodes');
+
+    // Call the grow function every 5 seconds; cells will duplicate
+    d3.interval(function() {
+        restart(grow(nodes));
+    }, 2000);
+
+    function restart(nodes) {
+        // transition
+        var t = d3.transition()
+            .duration(750);
+
+        node = node.data(nodes);
+
+        // Exit any nodes that don't make good data points
+        node.exit()
+            .style('fill', '#b26745')
+            .transition(t)
+            .attr('r, 1e-6')
+            .remove();
+
+        // Change existing nodes
+        node
+            .transition(t)
+            .style('fill', '#3a403d')
+            .attr('r', function(d) {return d.radius;});
+
+        // Transition in new nodes
+        node = node.enter().append('circle')
+            .style('fill', '#45b29d')
+            .attr('r', function(d) {return d.radius;})
+            .merge(node);
+
+        simulation.nodes(nodes);
+    }
+
+    // Required to update displayed position of nodes with force simulation
+    function ticked() {
+        node.attr('cx', function(d) {return d.x;})
+            .attr('cy', function(d) {return d.y;});
+    }
+
+    var splitprob = 0.90;
+
+    function grow() {
+        if (nodes.length < 200) {
+            for (i = 0; i < nodes.length; i++) {
+                if (Math.random() < splitprob) {
+                    nodes.push({radius: cellRadius});
+                }
+            }
+        }
+        return nodes;
+    }
+});
+
+/*
+$(document).ajaxStop(function() {
+    var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+    var width = 300,
+        height = 300;
+
+    var nodes = randomizeData();
+
+    var simulation = d3.forceSimulation(nodes)
+        .force("charge", d3.forceManyBody().strength(-150))
+        .force("forceX", d3.forceX().strength(.1))
+        .force("forceY", d3.forceY().strength(.1))
+        .force("center", d3.forceCenter())
+        .alphaTarget(1)
+        .on("tick", ticked);
+
+    var svg = d3.select(".petri").append("svg").attr("width", width).attr("height", height),
+    g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
+    node = g.append("g").attr("stroke", "#fff").attr("stroke-width", 1.5).selectAll(".node");
+
+    d3.interval(function(){
+        restart(randomizeData());
+    }, 2000);
+
+    function restart(nodes) {
+
+        // transition
+        var t = d3.transition()
+            .duration(750);
+
+        // Apply the general update pattern to the nodes.
+        node = node.data(nodes, function(d) { return d.name;});
+
+        node.exit()
+            .style("fill", "#b26745")
+            .transition(t)
+            .attr("r", 1e-6)
+            .remove();
+
+        node
+            .transition(t)
+            .style("fill", "#3a403d")
+            .attr("r", function(d){ return d.size; });
+
+        node = node.enter().append("circle")
+            .style("fill", "#45b29d")
+            .attr("r", function(d){ return d.size; })
+            .merge(node);
+
+        // Update and restart the simulation.
+        simulation.nodes(nodes)
+            .force("collide", d3.forceCollide().strength(1).radius(function(d){ return d.size + 10; }).iterations(1));
+
+    }
+
+    function ticked() {
+        node.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+
+    }
+
+    function randomizeData(){
+        var d0 = jz.arr.shuffle(alphabet),
+            d1 = [],
+            d2 = [];
+        for (var i = 0; i < jz.num.randBetween(1, alphabet.length); i++){
+            d1.push(d0[i]);
+        }
+        d1.forEach(function(d){
+            d2.push({name: d, size: jz.num.randBetween(0, 50)});
+        });
+        return d2;
+    }
     */
+    /*
+    $('.petri').css('height', $('.petri').css('width'));
 
     var interactive = d3.select(".petri");
 
@@ -25,8 +178,8 @@ $(document).ajaxStop(function() {
         .attr("width", dim)
         .attr("height", dim);
 
-    var nodes_data = d3.range(500).map(function(i) {
-        return {index: i, radius: (i%3)*5};
+    var nodes_data = d3.range(100).map(function(i) {
+        return {index: i, radius: 10};
     });
     //set up the simulation 
     //nodes only for now 
@@ -38,11 +191,41 @@ $(document).ajaxStop(function() {
     //also going to add a centering force
     simulation
         //.gravity(0.05)
-        .force("charge_force", d3.forceManyBody().strength(5))
-        .force("collision", d3.forceCollide(function(d) {return d.radius + 4;}))
+        .force("charge_force", d3.forceManyBody().strength(2).distanceMax(dim/4))
+        .force("collision", d3.forceCollide(function(d) {return d.radius + 2;}))
         .force("center_force", d3.forceCenter(dim / 2, dim / 2));
 
+    
 
+    var cells = petri.append("g")
+        .attr("class", "nodes");
+
+    //draw circles for the nodes 
+    var node = cells
+        .selectAll("circle")
+        .data(nodes_data)
+        .enter()
+        .append("circle")
+        .attr("r", function(d) {return d.radius;});
+        //.attr("fill", "red");
+        //.call(drag(simulation));
+
+    simulation.on("tick", tickActions );
+
+    var splitprob = 0.05;
+    var splitting = true;
+
+    function tickActions() {
+        //update circle positions each tick of the simulation 
+        node
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+    }
+});
+*/
+
+
+    /*
     var drag = simulation => {
         
         function dragstarted(d) {
@@ -67,149 +250,5 @@ $(document).ajaxStop(function() {
             .on("drag", dragged)
             .on("end", dragended);
     };
-    //draw circles for the nodes 
-    var node = petri.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle")
-        .data(nodes_data)
-        .enter()
-        .append("circle")
-        .attr("r", function(d) {return d.radius;})
-        .attr("fill", "red")
-        .call(drag(simulation));
-
-    simulation.on("tick", tickActions );
-
-    /*
-    //Create links data 
-    var links_data = [
-        {"source": "Travis", "target": "Rake"},
-        {"source": "Diana", "target": "Rake"},
-        {"source": "Diana", "target": "Rachel"},
-        {"source": "Rachel", "target": "Rake"},
-        {"source": "Rachel", "target": "Shawn"},
-        {"source": "Emerald", "target": "Rachel"}
-    ];
-
-    //Create the link force 
-    //We need the id accessor to use named sources and targets 
-    var link_force =  d3.forceLink(links_data)
-        .id(function(d) { return d.name; });
-
-
-    simulation.force("links",link_force);
-
-    //draw lines for the links 
-    var link = petri.append("g")
-        .attr("class", "links")
-        .selectAll("line")
-        .data(links_data)
-        .enter().append("line")
-        .attr("stroke-width", 2);
-
-        */
-
-    function tickActions() {
-        //update circle positions each tick of the simulation 
-        node
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
-        
-        /*
-        //update link positions 
-        //simply tells one end of the line to follow one node around
-        //and the other end of the line to follow the other node around
-        link
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-        */
-    }
-    /*
-    var gridSpacing = 10, // Repeat distance in pixels between cells
-        tick = 500,
-        Ny = Math.floor(dim / (gridSpacing * Math.sin(Math.PI / 3))),
-        Nx = Math.floor(dim / gridSpacing),
-        divprob = 0.1,
-        radius = gridSpacing/2 - 2,
-        grid = d3.map();
-
-    var d_inactive,
-        d_active;
-
-    var cells;
-
-    //init();
-    loadRandom(0.25);
-    draw();
-    
-    //var nodes = d3.range(200).map(function(i) { return {x: 50*(i % 10), y: 50*(Math.floor(i / 10)), radius: Math.random() * 18 + 5}; });
-
-    petri.selectAll("circle")
-        .data(cells)
-        .enter().append("svg:circle")
-        .attr("r", function(d) { return radius; })
-        .style("fill", function(d, i) { return '#ffffff'; })
-        .attr("cx", function(d) { return (d.x + 0.5 * (d.y % 2))* gridSpacing; })
-        .attr("cy", function(d) { return d.y * gridSpacing * Math.sin(Math.PI / 3); })
-        .on("mouseover", function(){d3.select(this).style("fill", "red");})
-        .on("mouseout", function(){d3.select(this).style("fill", "white");});
-
-    function loadRandom(p) {
-        var n = Math.floor((p || 0.05)*Nx*Ny), // How many cells are we loading in
-            i = d3.randomUniform(0,  Nx-1), // Randomly generate positions on the X axis
-            j = d3.randomUniform(1, Ny), // Randomly generate positions on the Y axis
-            d;
-
-        cells = d3.range(n).map(function(k) {
-            var x = Math.floor(i()) + 1,
-                y = Math.floor(j()) + 1;
-            return {x: x,
-                    y: y};
-        });
-    }
-
-    function step() {
-        if (cells.size() > 0) {
-            cells.each(function (cell, key) {
-
-            });
-        }
-    }
-
-    function draw() {
-    }
-
-    function add() { // TBI: Add a single cell
-
-    }
-
-    function neighbors(c) {
-        // Key:
-        // 0: [n - 1, m + 1]
-        // 1: [n, m + 1]
-        // 2: [n + 1, m]
-        // 3: [n, m - 1]
-        // 4: [n - 1, m - 1]
-        // 5: [n - 1, m]
-
-        var n = c[0],
-            m = c[1];
-        return [
-            cells[n - 1, m + 1],
-            cells[n, m + 1],
-            cells[n + 1, m],
-            cells[n, m - 1],
-            cells[n - 1, m - 1],
-            cells[n - 1, m]
-        ];
-    }
-
-    function redraw() {
-        step();
-        draw();
-        d3.timeout(redraw, tick);
-    }
     */
-});
+
